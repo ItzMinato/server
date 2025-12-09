2e565e7395f64be57d93af7461cdbe016a4bd582
sonichkaa1 <soniaskopina11.s@gmail.com>
2025-12-09 16:44:55 +0200

    Create Group

--- client-ui/src/main/java/org/example/ApiClient.java
+++ client-ui/src/main/java/org/example/ApiClient.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -59,7 +60,7 @@ public class ApiClient {
     public static List<Group> getUserGroups() throws Exception {
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(BASE_URL + "/groups"))  // <-- ВАЖЛИВО!!!
+                .uri(URI.create(BASE_URL + "/groups"))
                 .header("Content-Type", "application/json")
                 .GET()
                 .build();
@@ -70,5 +71,24 @@ public class ApiClient {
         Group[] groups = gson.fromJson(response.body(), Group[].class);
 
         return Arrays.asList(groups);
+    }
+
+    // ------------------ CREATE GROUP ---------------------
+
+    public static String createGroup(String name) throws Exception {
+
+        JsonObject json = new JsonObject();
+        json.addProperty("name", name);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/groups"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
     }
 }
